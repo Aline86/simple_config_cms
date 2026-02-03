3b9992b4483beef480cbab338993a7dc53e0300f
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-02-03 11:17:36 +0100

    page modification, adaptation if no blocs

--- app/edition/pages/PageClient.tsx
+++ app/edition/pages/PageClient.tsx
@@ -78,7 +78,7 @@ export default function PageClient({
       const result = pages.filter(
         (p) => p.number_page_position !== page.number_page_position,
       );
-
+      let i = 0;
       const result_pages = result.map((p: PageObject, index: number) => {
         return new PageObject(
           {
@@ -89,7 +89,7 @@ export default function PageClient({
             text_titre: p.text_titre ?? "", // text_titre vide
             text_description: p.text_description ?? "",
             slug: p.text_slug ?? "", // text_titre vide
-            number_page_position: index + 1,
+            number_page_position: i++,
             langue: p.text_langue ?? "fr_FR", // langue par défaut "fr"
             blocs: p.blocs ?? [], // aucun bloc par défaut
             text_createdAt: p.text_createdAt ?? new Date(),
