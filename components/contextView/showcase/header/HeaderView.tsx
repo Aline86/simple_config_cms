09f1793ee5c9b989580b45379c694e6e518ccf51
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-04-29 13:27:32 +0200

    adding missing auth + handling space in nav

--- components/contextView/showcase/header/HeaderView.tsx
+++ components/contextView/showcase/header/HeaderView.tsx
@@ -149,7 +149,7 @@ export default function HeaderView({ bloc }: ViewProps) {
                 )}
               </a>
 
-              <h1 className="flex-shrink-0 mx-4 sm:text-2xl md:text-3xl ">
+              <h1 className="flex-shrink-0 mx-4 sm:text-1xl md:text-3xl ">
                 {bloc.text_nom_site}
               </h1>
 
