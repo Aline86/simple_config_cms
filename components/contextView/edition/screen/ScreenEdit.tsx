d3b1aab669dc296a7b399ea2087a074edbe736a9
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-06-13 01:33:39 +0200

    remove some dupplication to get back A codacy score

--- components/contextView/edition/screen/ScreenEdit.tsx
+++ components/contextView/edition/screen/ScreenEdit.tsx
@@ -4,6 +4,7 @@ import { BlocObject } from "../../../../database/model/Bloc";
 import { MediaObject } from "../../../../database/model/bloc/MediaObject";
 import { EditorProps } from "../../../../lib/helpers/globabProps";
 import { FieldRenderer } from "../../../../lib/validators/renderer/TextRenderer";
+import HeadingComponent from "../_commons/HeadingComponent";
 import { PictureEditor } from "../grid/image_grid/PictureEditor";
 
 export default function ScreenEdit({
@@ -18,52 +19,29 @@ export default function ScreenEdit({
 }: EditorProps) {
   return (
     <section className="mx-auto w-full min-w-[44vw] space-y-6 p-6 mb-8">
-      <div className="space-y-2">
-        <h2 className="text-2xl font-bold tracking-tight text-slate-900 dark:text-slate-50">
-          Configuration du composant "Ecran"
-        </h2>
-        <p className="text-sm text-slate-500 dark:text-slate-400">
-          Configurez le contenu de votre composant
-        </p>
+      <HeadingComponent bloc={bloc} onChange={onChange} name={"Ecran"} />
+      <div
+        className={
+          bloc.image_medias.length === 1
+            ? "grid grid-cols-1 "
+            : "grid grid-cols-2 gap-6"
+        }
+      >
+        {bloc.image_medias.map((media) => {
+          return (
+            <PictureEditor
+              key={(media as MediaObject).id}
+              media={media as MediaObject}
+              onChange={onChange}
+              removeElement={removeElement}
+              onDragStart={onDragStart}
+              onDrop={onDrop}
+              isLink={false}
+              blocNumber={bloc.bloc_position}
+            />
+          );
+        })}
       </div>
-
-      <div className="rounded-lg border border-slate-200 bg-white p-6 shadow-sm dark:border-slate-200 dark:bg-slate-950">
-        <FieldRenderer
-          label="Titre de la carte de redirection"
-          fieldName={`blocs.` + bloc.bloc_position + `.` + `text_titre`}
-          model={bloc as BlocObject}
-          setField={onChange}
-        />
-        <FieldRenderer
-          label="text_description courte (texte court - 500 caractères)"
-          fieldName={`blocs.` + bloc.bloc_position + `.` + `text_description`}
-          model={bloc as BlocObject}
-          setField={onChange}
-        />
-        <div
-          className={
-            bloc.image_medias.length === 1
-              ? "grid grid-cols-1 "
-              : "grid grid-cols-2 gap-6"
-          }
-        >
-          {bloc.image_medias.map((media) => {
-            return (
-              <PictureEditor
-                key={(media as MediaObject).id}
-                media={media as MediaObject}
-                onChange={onChange}
-                removeElement={removeElement}
-                onDragStart={onDragStart}
-                onDrop={onDrop}
-                isLink={false}
-                blocNumber={bloc.bloc_position}
-              />
-            );
-          })}
-        </div>
-      </div>
-
       {/* Debug panel */}
       {show_debug ? (
         <div className="rounded-lg border border-slate-200 bg-slate-50 p-4 dark:border-slate-200 dark:bg-slate-900">
--- components/contextView/edition/video/VideoEdit.tsx
+++ components/contextView/edition/video/VideoEdit.tsx
@@ -3,6 +3,7 @@
 import { BlocObject } from "../../../../database/model/Bloc";
 import { MediaObject } from "../../../../database/model/bloc/MediaObject";
 import { FieldRenderer } from "../../../../lib/validators/renderer/TextRenderer";
+import HeadingComponent from "../_commons/HeadingComponent";
 import { VideoEditor } from "./VideoEditor";
 
 interface EditorProps {
@@ -27,50 +28,28 @@ export default function VideoEdit<T>({
 }: EditorProps) {
   return (
     <section className="mx-auto min-w-2xl max-w-2xl space-y-6 p-6 mb-8">
-      <div className="space-y-2">
-        <h2 className="text-2xl font-bold tracking-tight text-slate-900 dark:text-slate-50">
-          Configuration du composant "Vidéo Youtube"
-        </h2>
-        <p className="text-sm text-slate-500 dark:text-slate-400">
-          Configurez le contenu de votre composant
-        </p>
-      </div>
-
-      <div className="rounded-lg border border-slate-200 bg-white p-6 shadow-sm dark:border-slate-200 dark:bg-slate-950">
-        <FieldRenderer
-          label="Titre de la carte de redirection"
-          fieldName={`blocs.` + bloc.bloc_position + `.` + `text_titre`}
-          model={bloc as BlocObject}
-          setField={onChange}
-        />
-        <FieldRenderer
-          label="text_description courte (texte court - 500 caractères)"
-          fieldName={`blocs.` + bloc.bloc_position + `.` + `text_description`}
-          model={bloc as BlocObject}
-          setField={onChange}
-        />
-        <div
-          className={
-            bloc.image_medias.length === 1
-              ? "grid grid-cols-1 "
-              : "grid grid-cols-2 gap-6"
-          }
-        >
-          {bloc.image_medias.map((media) => {
-            return (
-              <VideoEditor
-                key={(media as MediaObject).id}
-                media={media as MediaObject}
-                onChange={onChange}
-                removeElement={removeElement}
-                onDragStart={onDragStart}
-                onDrop={onDrop}
-                isLink={isLink}
-                blocNumber={bloc.bloc_position}
-              />
-            );
-          })}
-        </div>
+      <HeadingComponent bloc={bloc} onChange={onChange} name={"Vidéo"} />
+      <div
+        className={
+          bloc.image_medias.length === 1
+            ? "grid grid-cols-1 "
+            : "grid grid-cols-2 gap-6"
+        }
+      >
+        {bloc.image_medias.map((media) => {
+          return (
+            <VideoEditor
+              key={(media as MediaObject).id}
+              media={media as MediaObject}
+              onChange={onChange}
+              removeElement={removeElement}
+              onDragStart={onDragStart}
+              onDrop={onDrop}
+              isLink={isLink}
+              blocNumber={bloc.bloc_position}
+            />
+          );
+        })}
       </div>
 
       {/* Debug panel */}
