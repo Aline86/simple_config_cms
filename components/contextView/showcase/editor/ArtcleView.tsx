0a3cbca4318095aaa8892a501195e59e5b596e3f
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-08-12 11:59:43 +0200

    fix finishing pre-render

--- components/contextView/showcase/editor/ArtcleView.tsx
+++ components/contextView/showcase/editor/ArtcleView.tsx
@@ -1,40 +1,39 @@
 "use client";
 
+import { useMemo } from "react";
+import { JSONContent } from "@tiptap/core";
+import { output } from "../../../../lib/helpers/tiptapFunctions";
+import AnimatedTitle from "../../../ui/animations/AnimatedTitle";
 
-  import { useMemo } from "react";
-  import type { JSONContent } from "@tiptap/core";
-  import { output } from "../../../../lib/helpers/tiptapFunctions";
-  import AnimatedTitle from "../../../ui/animations/AnimatedTitle";
+interface BlocParams {
+  index: number;
+  bloc: JSONContent;
+}
 
-  interface BlocParams {
-    index: number;
-    bloc: JSONContent;
-  }
+function ArticleView({ bloc }: BlocParams) {
+  const blocks = useMemo(() => {
+    if (!bloc) return [];
+    return output(bloc) ?? [];
+  }, [bloc]);
 
-  function ArticleView({ bloc }: BlocParams) {
-    const blocks = useMemo(() => {
-      if (!bloc) return [];
-      return output(bloc) ?? [];
-    }, [bloc]);
+  return (
+    <section className="w-full mb-8">
+      {blocks.map((block, index) => {
+        const content = (
+          <div
+            className="tiptap none"
+            dangerouslySetInnerHTML={{ __html: block.html }}
+          />
+        );
 
-    return (
-      <section className="w-full mb-8">
-        {blocks.map((block, index) => {
-          const content = (
-            <div
-              className="tiptap none"
-              dangerouslySetInnerHTML={{ __html: block.html }}
-            />
-          );
+        if (block.type === "heading") {
+          return <AnimatedTitle key={index}>{content}</AnimatedTitle>;
+        }
 
-          if (block.type === "heading") {
-            return <AnimatedTitle key={index}>{content}</AnimatedTitle>;
-          }
-
-          return <div key={index}>{content}</div>;
-        })}
-      </section>
-    );
-  }
+        return <div key={index}>{content}</div>;
+      })}
+    </section>
+  );
+}
 
 export default ArticleView;
