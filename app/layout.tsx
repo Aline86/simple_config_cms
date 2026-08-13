e0f9c7615f8fa6aba82497d116184fcba062b6bc
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-08-13 11:02:41 +0200

    adding analytics script and shortening footer size

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,17 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="fr">
       <head>
-        {process.env.UMAMI_ENV === "demo" && (
+        {process.env.UMAMI_ENV === "demo" ? (
           <script
             defer
             src="https://umami-nine-ruby.vercel.app/script.js"
             data-website-id="cc90c567-2eaf-498a-ac92-1e532a1f308d"
+          ></script>
+        ) : (
+          <script
+            defer
+            src="https://umami-nine-ruby.vercel.app/script.js"
+            data-website-id="10a543ee-e8b2-4299-ac27-dbd7bff38a96"
           ></script>
         )}
       </head>
