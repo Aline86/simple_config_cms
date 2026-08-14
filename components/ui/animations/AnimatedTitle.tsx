2f5dce581382d0f5472e3de3b305fb2f54e94044
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-08-14 21:52:07 +0200

    putting previous animation

--- components/ui/animations/AnimatedTitle.tsx
+++ components/ui/animations/AnimatedTitle.tsx
@@ -10,16 +10,14 @@ export default function AnimatedTitle({
   className?: string;
 }) {
   return (
-    <div className="overflow-hidden">
-      <motion.div
-        initial={{ opacity: 0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: false, margin: "60px" }}
-        transition={{ duration: 0.7, ease: [0.22, 1, 0.36, 1] }}
-        className={className}
-      >
-        {children}
-      </motion.div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false, margin: "-60px" }}
+      transition={{ duration: 0.7, ease: [0.22, 1, 0.36, 1] }}
+      className={className}
+    >
+      {children}
+    </motion.div>
   );
 }
