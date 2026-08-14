447da9c1deaaa8ec79ff1c718a59fc2b3b584124
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-08-14 21:18:12 +0200

    fix iphone screen view compatibility

--- components/ui/animations/AnimatedTitle.tsx
+++ components/ui/animations/AnimatedTitle.tsx
@@ -13,7 +13,7 @@ export default function AnimatedTitle({
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: false, margin: "60px" }}
+      viewport={{ once: true, margin: "60px" }}
       transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
       className={className}
     >
