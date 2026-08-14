e81b7a8bdb6046587ef735be1d5d95ed7dcf5c4e
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-08-14 21:13:49 +0200

    fix iphone screen view compatibility

--- components/ui/animations/AnimatedTitle.tsx
+++ components/ui/animations/AnimatedTitle.tsx
@@ -10,16 +10,14 @@ export default function AnimatedTitle({
   className?: string;
 }) {
   return (
-    <div className="overflow-hidden">
-      <motion.div
-        initial={{ opacity: 0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, amount: 0.3 }}
-        transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
-        className={className}
-      >
-        {children}
-      </motion.div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.3 }}
+      transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
+      className={className}
+    >
+      {children}
+    </motion.div>
   );
 }
