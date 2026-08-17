b081105098e70662622db35adb899ac24b6c0f92
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-08-17 23:25:25 +0200

    fixing titles

--- components/contextView/showcase/header/NavBarView.tsx
+++ components/contextView/showcase/header/NavBarView.tsx
@@ -30,7 +30,7 @@ export default function NavBarView({
     bloc.mode === "edition"
       ? ""
       : measured
-        ? "shadow fixed top-0 left-0 right-0 z-20"
+        ? "shadow fixed top-0 left-0 right-0 z-20 pr-2"
         : "hidden";
   const mode_nav =
     bloc.mode === "edition" ? "mx-auto px-4 w-[45vw] top-[40vh]" : "mx-auto ";
