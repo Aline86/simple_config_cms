7584a2d6e1fdcbb1d0991f5ba342ff5969cc6efd
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-08-18 08:57:36 +0200

    fixing titles

--- components/ui/animations/AnimatedTitle.tsx
+++ components/ui/animations/AnimatedTitle.tsx
@@ -11,7 +11,7 @@ export default function AnimatedTitle({
 }) {
   return (
     <motion.div
-      initial={{ opacity: 0, y: -20 }}
+      initial={{ opacity: 0, y: -40 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: false, margin: "60px" }}
       transition={{ duration: 0.7, ease: [0.22, 1, 0.36, 1] }}
